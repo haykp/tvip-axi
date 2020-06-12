d46357e4c6812c5fdcdf0232ae435ba54459bd33
haykp <haykpet@gmail.com>
2020-06-12 22:42:19 +0900

    Update findTaskUsage.py

--- Python/findTaskUsage.py
+++ Python/findTaskUsage.py
@@ -1,7 +1,33 @@
 
 ## Execute Anadonda powershell Promt
-## cd D:\Documents\AXI_VIP\dev\Python
+## cd D:\Documents\AXI_VIP\dev\tvip-axi\Python
 ## cd D:\Documents\AXI_VIP\dev\Python
 
 
 print ("Getting function or task usage in the file")
+
+class ParseFunctionsTasks (object):
+
+	def __init__(self, namesList="namesList"):
+		self.namesList=namesList
+		
+	def readNamesList (self):
+		with open(self.namesList) as f:
+			self.checkNames = f.read()	
+			print (self.checkNames)
+	
+	def getFunctions (self):
+		#for each in self.checkNames:
+		#	print (each)
+		return
+	
+	
+	
+#### Main	
+print ("=========== Main =================")
+
+objNamesParser=ParseFunctionsTasks("taskFunctionsList.txt")
+objNamesParser.readNamesList()
+objNamesParser.getFunctions()
+
+
