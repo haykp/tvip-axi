49529347634c8cc47e40823288abf86c9fb8b4bb
haykp <haykpet@gmail.com>
2020-06-17 23:06:22 +0900

    developing another class

--- Python/findTaskUsage.py
+++ Python/findTaskUsage.py
@@ -12,31 +12,50 @@ class ParseFunctionsTasks (object):
 	def __init__(self, namesList="namesList"):
 		self.namesList=namesList
 		
-	def readNamesList (self):
+	def readNamesList(self):
 		with open(self.namesList) as f:
 			self.func_taskNames = f.read().splitlines()	
 		#	print (self.func_taskNames)
 	
-	def getFunctions (self):
+	def getFunctions(self):
 		#protected virtual function bit get_write_data_last_value(bit valid);
-		
+		resList=[]
 		for each in self.func_taskNames:
 			#print (each)
 			m=re.match(r".+\s+function\s+\w+\s+(\w+)\s*\(.+$", each)
 			if m:
+#				print ((each), "==>", m.group(1) )
+				resList.append(m.group(1))
+				
+		print ("==> Functions <== \n", resList)		
+		return resList
+		
+	def getTasks(self):
+		#protected virtual function bit get_write_data_last_value(bit valid);
+		resList=[]
+		for each in self.func_taskNames:
+			#print (each)
+			m=re.match(r".+\s+task\s+(\w+)\s*\(.+$", each)
+			if m:
 				print ((each), "==>", m.group(1) )
-
+				resList.append(m.group(1))
+				
+		print ("==> Tasks <== \n", resList)		
+		return resList	
+	
+class getCall :
+		
 			
-			
-		return
-	
-	
-	
+		def __init__(self, namesList="namesList"):
+			self.objNamesParser=ParseFunctionsTasks(namesList)
+		
+		
 #### Main	
 print ("=========== Main =================")
 
-objNamesParser=ParseFunctionsTasks("taskFunctionsList.txt")
+objNamesParser=ParseFunctionsTasks("tvip_axi_component_baseFunctions.txt")
 objNamesParser.readNamesList()
 objNamesParser.getFunctions()
+objNamesParser.getTasks()
 
 
