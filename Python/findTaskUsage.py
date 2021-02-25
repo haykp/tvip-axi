a7b2a1f05fe25e674263fd8aeb5f35f55e9991c2
haykp <haykpet@gmail.com>
2021-02-25 10:22:24 +0900

    Update findTaskUsage.py

--- Python/findTaskUsage.py
+++ Python/findTaskUsage.py
@@ -43,19 +43,96 @@ class ParseFunctionsTasks (object):
 		print ("==> Tasks <== \n", resList)		
 		return resList	
 	
-class getCall :
+class getCall (object) :
 		
 			
-		def __init__(self, namesList="namesList"):
+		def __init__(self, namesList="namesList",systemVerilogFile="fileText" ):
 			self.objNamesParser=ParseFunctionsTasks(namesList)
+			self.objNamesParser.readNamesList()
+			self.svFile=systemVerilogFile
+			
 		
+		def start(self):
+			self.functions= self.objNamesParser.getFunctions()
+			self.tasks= self.objNamesParser.getTasks()
+			return self.functions+self.tasks
 		
+		def readSVFile (self):
+			with open(self.svFile) as f:
+				self.svData = f.read().splitlines()			
+			print (self.svData)
+		
+		def findFunctionsUsage(self):
+		
+			notUsedList=[]
+			usedList=[]
+			
+			print ("Size of self.functions:", len(self.functions))
+			i=0;
+			
+			for function in self.functions:
+				#print (function)
+				used=0;
+				for line in self.svData:
+					if function in line and ( not used):
+						print ("Used:", function, "=> RTL=", line, "  :i=", i)
+						i=i+1
+						used=1;
+						usedList.append(function)
+				
+				if (not used):
+					notUsedList.append(function)
+				
+			print ("========== Used Functions ===============\n")
+			print (usedList)
+			print ("========== Not Used Functions ===============\n")
+			print (notUsedList)
+
+		def findTasksUsage(self):
+		
+			notUsedList=[]
+			usedList=[]
+			
+			print ("Size of self.tasks:", len(self.tasks))
+			i=0;
+			
+			for task in self.tasks:
+				#print (function)
+				used=0;
+				for line in self.svData:
+					if task in line and ( not used):
+						print ("Used:", task, "=> RTL=", line, "  :i=", i)
+						i=i+1
+						used=1;
+						usedList.append(task)
+				
+				if (not used):
+					notUsedList.append(task)
+			print ("========== Used Tasks ===============\n")
+			print (usedList)
+			print ("========== Not Used Tasks ===============\n")
+			print (notUsedList)
+			
 #### Main	
 print ("=========== Main =================")
 
-objNamesParser=ParseFunctionsTasks("tvip_axi_component_baseFunctions.txt")
-objNamesParser.readNamesList()
-objNamesParser.getFunctions()
-objNamesParser.getTasks()
+#objNamesParser=ParseFunctionsTasks("tvip_axi_component_baseFunctions.txt")
+#objNamesParser.readNamesList()
+#objNamesParser.getFunctions()
+#objNamesParser.getTasks()
+
+#getCallObj = getCall(namesList="tvip_axi_component_baseFunctions.txt",
+#					 systemVerilogFile="tvip_axi_master_write_driver.svh")
+					 
+#getCallObj = getCall(namesList="tvip_axi_component_baseFunctions.txt",
+#					 systemVerilogFile="tvip_axi_slave_driver.svh")
+					 
+getCallObj = getCall(namesList="tvip_axi_component_baseFunctions.txt",
+					 systemVerilogFile="tvip_axi_monitor_base.svh")					 
+					 
+getCallObj.start()
+getCallObj.readSVFile()
+getCallObj.findFunctionsUsage()
+getCallObj.findTasksUsage()
 
 
